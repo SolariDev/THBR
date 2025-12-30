d6b92704cabdf5b5ecae324947bb837980215530
SolariDev <gabrielsolari75@gmail.com>
2025-12-30 10:38:43 -0300

    Ajuste visual en inicio

--- themes/hello-elementor/page-inicio.php
+++ themes/hello-elementor/page-inicio.php
@@ -5,18 +5,18 @@ Template Name: Inicio
 get_header();
 ?>
 
-<div class="thbr-home">
+<div class="thbr-botones-centrados" style="margin-top:20px;">
   <!-- Logo institucional -->
-  <img src="<?php echo plugins_url( 'assets/logobrthbr.png', WP_PLUGIN_DIR . '/thbr/index.php' ); ?>" alt="Logo TreeHouse" />
+  <img src="<?php echo plugins_url( 'assets/logobrthbr.png', WP_PLUGIN_DIR . '/thbr/index.php' ); ?>" alt="Logo TreeHouse" style="max-width:280px; margin-bottom:0px;"/>
 
   <!-- Misión institucional -->
-  <p class="thbr-intro">
+  <p class="thbr-intro" style="margin-top:0; margin-bottom:10px; line-height:1.4;">
     En TreeHouse Bienes Raíces trabajamos para ofrecer un servicio de calidad,<br>
     gestionando propiedades con compromiso, transparencia y cercanía.
   </p>
 
   <!-- Botones de acción -->
-  <div>
+  <div class="grupo-botones">
     <a href="<?php echo home_url('/registro'); ?>" class="thbr-btn">Registro</a>
     <a href="<?php echo home_url('/ingresar'); ?>" class="thbr-btn">Iniciar sesión</a>
   </div>
